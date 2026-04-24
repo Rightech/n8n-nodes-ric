4cc0d8441c7fc77ad114f1f974d07e549e3f1a7f
l.tarasov <l.tarasov@rightech.io>
2026-04-25 01:08:51 +0300

    fixed type issue seen in build in utils signature

--- nodes/RightechIotCloud/common/util.ts
+++ nodes/RightechIotCloud/common/util.ts
@@ -2,7 +2,7 @@
  * Cast selected keys to lowercase for CI substring search
  * todo: go over use cases and convert to fuzzy API search when/if it becomes available
  */
-export function toSearchable<T extends object>(item: T, ...fields: string[]): T & { _search: string } {
+export function toSearchable<T extends object>(item: T, ...fields: (keyof T)[]): T & { _search: string } {
     const _search = fields
         .map(field => field in item ? String(item[field]).toLowerCase() : '')
         .filter(part => part !== '')
