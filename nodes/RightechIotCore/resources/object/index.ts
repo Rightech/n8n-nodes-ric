c95d2c3b9c10485184f3de1484370bd3907f5dc5
Lev Tarasov <l.tarasov@rightech.io>
2026-05-10 02:05:28 +0300

    refactor: moved command props outside of index object params for clarity

--- nodes/RightechIotCore/resources/object/index.ts
+++ nodes/RightechIotCore/resources/object/index.ts
@@ -1,13 +1,13 @@
 import type {INodeProperties} from 'n8n-workflow';
 import {modelSelector, objectSelector, stdQueryParameters} from "../../common/properties.js";
 import {get} from "./get.js";
-import {sendCommand} from "./sendCommand.js";
 import {handlerFn} from "../../common/types.js";
 import {getMany} from "./getMany.js";
 import {update, objectUpdateProperties} from "./update.js";
 import {create, objectCreateProperties} from "./create.js";
 import {getHistory, objectGetHistoryProperties} from "./getHistory.js";
 import {getEvents, objectGetEventsProperties} from "./getEvents.js";
+import {sendCommand, objectSendCommandProperties} from "./sendCommand.js";
 
 export const object: Record<string, handlerFn> = {get, create, update, getMany, getHistory, getEvents, sendCommand};
 
@@ -74,77 +74,9 @@ export const objectApiProperties: INodeProperties[] = [
         displayOptions: {
             show: {
                 resource: ['object'],
-                operation: ['get', 'sendCommand'],
+                operation: ['get'],
             },
         },
-    },
-    {
-        displayName: 'Command ID',
-        name: 'commandId',
-        required: true,
-        type: 'resourceLocator',
-        default: {
-            mode: 'list',
-            value: '',
-        },
-        modes: [
-            {
-                displayName: 'From List',
-                name: 'list',
-                type: 'list',
-                placeholder: 'Select a command...',
-                hint: 'Commands are tied to the object model, so you need to select an object first.',
-                typeOptions: {
-                    searchListMethod: 'listCommands',
-                    searchable: true,
-                    searchFilterRequired: false,
-                },
-            },
-            {
-                displayName: 'By ID',
-                name: 'id',
-                type: 'string',
-                placeholder: 'e.g. led-on',
-                hint: 'You can find command IDs in the object model declaration tree in RIC web UI - typically, you would look for a "Commands" folders and then at contained objects.',
-                validation: [
-                    {
-                        type: 'regex',
-                        properties: {
-                            regex: '^[a-z0-9-_]$',
-                            errorMessage: 'Command ID is an alphanumeric string.',
-                        },
-                    },
-                ],
-            },
-        ],
-        displayOptions: {
-            show: {
-                resource: ['object'],
-                operation: ['sendCommand'],
-            },
-        },
-    },
-    {
-        displayName: 'Command Options',
-        name: 'commandOptions',
-        type: 'collection',
-        placeholder: 'Add Field',
-        default: {},
-        displayOptions: {
-            show: {
-                resource: ['object'],
-                operation: ['sendCommand'],
-            },
-        },
-        options: [
-            {
-                displayName: 'Command Data',
-                name: 'commandAuxiliaryData',
-                hint: 'Sent data must match with declared parametric fields, the entire object will be sent as is.',
-                type: 'json',
-                default: {},
-            },
-        ],
     },
     {
         ...modelSelector,
@@ -232,4 +164,5 @@ export const objectApiProperties: INodeProperties[] = [
     ...objectCreateProperties,
     ...objectUpdateProperties,
     ...objectGetEventsProperties,
+    ...objectSendCommandProperties,
 ];
--- nodes/RightechIotCore/resources/object/sendCommand.ts
+++ nodes/RightechIotCore/resources/object/sendCommand.ts
@@ -1,6 +1,79 @@
-import {GenericValue, IDataObject, IExecuteFunctions, INodeExecutionData} from "n8n-workflow";
+import {GenericValue, IDataObject, IExecuteFunctions, INodeExecutionData, INodeProperties} from "n8n-workflow";
 import {INodeParameterResourceLocator} from "n8n-workflow/dist/esm/interfaces.js";
 import {httpCall} from "../../common/util.js";
+import {objectSelector} from "../../common/properties.js";
+
+const displayOptions = {
+    show: {
+        resource: ['object'],
+        operation: ['sendCommand'],
+    },
+};
+
+export const objectSendCommandProperties: INodeProperties[] = [
+    {
+        ...objectSelector,
+        displayOptions,
+    },
+    {
+        displayName: 'Command ID',
+        name: 'commandId',
+        required: true,
+        type: 'resourceLocator',
+        default: {
+            mode: 'list',
+            value: '',
+        },
+        modes: [
+            {
+                displayName: 'From List',
+                name: 'list',
+                type: 'list',
+                placeholder: 'Select a command...',
+                hint: 'Commands are tied to the object model, so you need to select an object first.',
+                typeOptions: {
+                    searchListMethod: 'listCommands',
+                    searchable: true,
+                    searchFilterRequired: false,
+                },
+            },
+            {
+                displayName: 'By ID',
+                name: 'id',
+                type: 'string',
+                placeholder: 'e.g. led-on',
+                hint: 'You can find command IDs in the object model declaration tree in RIC web UI - typically, you would look for a "Commands" folders and then at contained objects.',
+                validation: [
+                    {
+                        type: 'regex',
+                        properties: {
+                            regex: '^[a-z0-9-_]$',
+                            errorMessage: 'Command ID is an alphanumeric string.',
+                        },
+                    },
+                ],
+            },
+        ],
+        displayOptions,
+    },
+    {
+        displayName: 'Command Options',
+        name: 'commandOptions',
+        type: 'collection',
+        placeholder: 'Add Field',
+        default: {},
+        displayOptions,
+        options: [
+            {
+                displayName: 'Command Data',
+                name: 'commandAuxiliaryData',
+                hint: 'Sent data must match with declared parametric fields, the entire object will be sent as is.',
+                type: 'json',
+                default: {},
+            },
+        ],
+    },
+];
 
 export async function sendCommand(exec: IExecuteFunctions, index: number): Promise<INodeExecutionData[]> {
     const objectId = exec.getNodeParameter('objectId', index) as INodeParameterResourceLocator;
